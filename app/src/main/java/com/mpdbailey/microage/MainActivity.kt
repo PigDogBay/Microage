11a1430d0fc07ab16741b5844db8061ba43b6f1f
Mark Bailey <markpdbailey@gmail.com>
2021-06-30 14:52:53 +0100

    Created fragments for main screens

--- app/src/main/java/com/mpdbailey/microage/MainActivity.kt
+++ app/src/main/java/com/mpdbailey/microage/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mpdbailey.microage
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.mpdbailey.microage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if (supportFragmentManager.findFragmentById(R.id.main_fragment_container)==null){
+            showHome()
+        }
+    }
+
+    fun showHome(){
+        replaceMainFragment(HomeFragment())
+    }
+
+    private fun replaceMainFragment(fragment: Fragment) {
+        val manager = supportFragmentManager
+        manager
+            .beginTransaction()
+            .replace(R.id.main_fragment_container, fragment)
+            .commit()
     }
 }
