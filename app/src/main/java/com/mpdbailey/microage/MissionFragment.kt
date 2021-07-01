c3be52315add8f5caf86a45a27f725674c51ce32
Mark Bailey <markpdbailey@gmail.com>
2021-07-01 14:14:17 +0100

    Mission pages created

--- app/src/main/java/com/mpdbailey/microage/MissionFragment.kt
+++ app/src/main/java/com/mpdbailey/microage/MissionFragment.kt
@@ -23,11 +23,24 @@ class MissionPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(act
     override fun getItemCount() = 11
 
     override fun createFragment(position: Int): Fragment {
-        return MissionPageFragment(R.layout.fragment_mission_home)
+        val layoutId = when (position){
+            1 -> R.layout.fragment_mission_1
+            2 -> R.layout.fragment_mission_2
+            3 -> R.layout.fragment_mission_3
+            4 -> R.layout.fragment_mission_4
+            5 -> R.layout.fragment_mission_5
+            6 -> R.layout.fragment_mission_6
+            7 -> R.layout.fragment_mission_7
+            8 -> R.layout.fragment_mission_8
+            9 -> R.layout.fragment_mission_9
+            10 -> R.layout.fragment_mission_10
+            else -> R.layout.fragment_mission_home
+        }
+        return MissionPageFragment(layoutId)
     }
 }
 
-class MissionPageFragment(val layoutId : Int) : Fragment() {
+class MissionPageFragment(private val layoutId : Int) : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(layoutId, container, false)
     }
