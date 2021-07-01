95c9be061bbb0ca992ff42989b9266a2e0b1ec83
Mark Bailey <markpdbailey@gmail.com>
2021-07-01 14:04:14 +0100

    ViewPager2 for mission fragment pages

--- app/src/main/java/com/mpdbailey/microage/MissionFragment.kt
+++ app/src/main/java/com/mpdbailey/microage/MissionFragment.kt
@@ -5,10 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.mpdbailey.microage.databinding.FragmentMissionBinding
 
 
 class MissionFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_mission, container, false)
+    private lateinit var binding : FragmentMissionBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentMissionBinding.inflate(layoutInflater)
+        binding.missionPager.adapter = MissionPagerAdapter(requireActivity())
+        return binding.root
+    }
+}
+
+class MissionPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount() = 11
+
+    override fun createFragment(position: Int): Fragment {
+        return MissionPageFragment(R.layout.fragment_mission_home)
+    }
+}
+
+class MissionPageFragment(val layoutId : Int) : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(layoutId, container, false)
     }
 }
